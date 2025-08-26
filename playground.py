7c9668ea9c062f02e78dbec0c149e7939092af7a
Gregg Reese <gr126108@student.wpsok.org>
2025-08-26 13:42:24 -0500

    completed hw3 and updated playground

--- playground.py
+++ playground.py
@@ -1,8 +1,6 @@
 import random
-magic_8_ball = ['yes','no','maybe','uncertain','try again','your code has failed kys']
-result=(random.choice(magic_8_ball))
-print(result)
-if result == 'try again':
-    print("yes or no")
-else:
-        pass
+
+def game():
+    if 
+    magic8ball = ('yes', 'no', 'maybe', 'try again')
+    print(random.choice(magic8ball))
