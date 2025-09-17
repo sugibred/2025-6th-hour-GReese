763f715aa766c39ff199d13cd37b71f0e39e992a
Gregg Reese <gr126108@student.wpsok.org>
2025-09-17 14:13:04 -0500

    updated playground.py

--- playground.py
+++ playground.py
@@ -1,6 +1,17 @@
 import random
 
-def game():
-    if 
-    magic8ball = ('yes', 'no', 'maybe', 'try again')
-    print(random.choice(magic8ball))
+
+yn_answer=input('Do you want to play a game?:')
+while True:
+    if yn_answer == 'yes':
+        rolled_die1=random.randint(1,6)
+        print(rolled_die1)
+        if rolled_die1 == 1:
+            print('you dropped the die in a drain')
+            quit()
+        else:
+            answerplyagn=input('Play Again?:')
+            if answerplyagn == 'yes':
+                continue
+            else:
+                exit()
